d81f04fbffbc452a09f131a8b5e57aac5028b08b
FoxZhao <395381480@qq.com>
2016-11-29 16:53:13 +0800

    首页API完成

--- app/apis/v1/home_page_api.rb
+++ app/apis/v1/home_page_api.rb
@@ -1,25 +1,38 @@
 module V1
   class HomePageAPI < Grape::API
-    namespace :home do
-      get 'brands' do
-        brands_with_products = Brand.where(is_show: true).includes(:products).order(:sort_order)
-        present brands_with_products, with: BrandsProductsEntity
-      end
-
-      get 'brands_all' do
-        brands = Brand.all.order(:sort_order)
+    namespace :home_page do
+      get '' do
+        brands_with_products = Brand.includes(:products).order(:sort_order)
         products_recommended = Product.where(is_recommended: true)
+        advertisements = Advertisement.all.order(:position)
         return_hash = {}
-        return_hash['brands'] = []
-        brands.each do |brand|
-          return_hash['brands'].append({id: brand.id, cn_name: brand.cn_name, logo: brand.logo})
+        return_hash['brands'] = {}
+        return_hash['brands']['is_show'] = []
+        i = 0
+        brands_with_products.each do |brand|
+          if brand.is_show
+            return_hash['brands']['is_show'].append(brand.entity)
+            return_hash['brands']['is_show'][i]['products'] = []
+            brand.products.each do |product|
+            return_hash['brands']['is_show'][i]['products'].append(product.entity)
+            end
+            i = i + 1
+          end
+        end
+        return_hash['brands']['all'] = []
+        brands_with_products.each do |brand|
+          return_hash['brands']['all'].append(brand.entity)
         end
         return_hash['recommended_cars'] = []
         products_recommended.each do |product_recommended|
-          return_hash['recommended_cars'].append({ id: product_recommended.id, name: product_recommended.name })
+          return_hash['recommended_cars'].append(product_recommended.entity)
+        end
+        return_hash['advertisements'] = []
+        advertisements.each do |advertisement|
+          return_hash['advertisements'].append(advertisement.entity)
         end
         return_hash
       end
     end
-  end  
+  end
 end
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -4,4 +4,9 @@ class Brand < ActiveRecord::Base
   validates :en_name, length: 2..16, allow_nil: true
   validates :cn_name, length: 2..16, uniqueness: true
   validates_presence_of :cn_name, :logo, :sort_order
+
+  def entity
+    { id: id, cn_name: cn_name, logo: logo, 
+      sort_order: sort_order, is_show: is_show }
+  end
 end
