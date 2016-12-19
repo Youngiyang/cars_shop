4c272ecd63b1cf127e3b16c358e7fa29cf1af754
FoxZhao <395381480@qq.com>
2016-12-19 15:34:15 +0800

    detail api

--- app/apis/v1/car_details_api.rb
+++ app/apis/v1/car_details_api.rb
@@ -2,35 +2,14 @@ module V1
   class CarDetailsAPI < Grape::API  
     namespace :details do
       get ':id' do
-        good = Good.includes(product: { goods: [{ good_spec_options: { spec_option: :spec } }, { good_attr_options: { attr_option: { attr: :attr_group } } }] }).find(params[:id])
+        good = Good.includes(product: { goods: [{ good_spec_options: { spec_option: :spec } }, { good_attr_options: { attr_option: :attr } }] }).find(params[:id])
         product = good.product
         goods = product.goods
         return_hash = {}
         return_hash['product'] = product.entity
+        return_hash['goods'] = []
         goods.each do |good|
-          
-        end
-      end
-      get ':id/specs' do
-        good = Good.includes(product: { goods: [{ good_spec_options: { spec_option: :spec } }, { good_attr_options: { attr_option: { attr: :attr_group } } }] }).find(params[:id])
-        product = good.product
-        goods = product.goods
-        return_hash = {}
-        return_hash['product_id'] = product.id
-        goods.each do |good|
-          good_spec_options = good.good_spec_options
-          good_spec_options.each do |good_spec_option|
-            spec_option = good_spec_option.spec_option
-            spec = spec_option.spec.name
-            if !return_hash.key?(spec)
-              return_hash[spec] = []
-              return_hash[spec].append(spec_option.value)
-            else
-              unless return_hash[spec].include?(spec_option.value)
-                return_hash[spec].append(spec_option.value)
-              end
-            end
-          end
+          return_hash['goods'].append(good.entity)
         end
         return_hash
       end
