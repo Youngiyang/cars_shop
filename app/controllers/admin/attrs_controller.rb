5f8c3d7ec4b5845fabf440dac3a16305238a17e4
杨志强 <yangzhiqiang@My-Wifi.local>
2016-12-20 17:01:56 +0800

    页面传递参数

--- app/controllers/admin/attrs_controller.rb
+++ app/controllers/admin/attrs_controller.rb
@@ -1,2 +1,15 @@
 class Admin::AttrsController < ApplicationController
+  def create
+    attr = Attr.new(attr_params)
+      if attr.save
+        render json: attr
+      else
+
+      end
+  end
+
+  private
+  def attr_params
+    params.require(:attr).permit(:key, :group_num)
+  end
 end
--- app/controllers/admin/goods_controller.rb
+++ app/controllers/admin/goods_controller.rb
@@ -9,5 +9,32 @@ class Admin::GoodsController < ApplicationController
     @brand = Brand.find(params[:brand_id])
     @product = Product.find(params[:product_id])
     @good = Good.new
+    # 基本配置
+    @attrs_one = Attr.get_gruop_attr(0).where(is_default: true)
+    #车身
+    @attrs_two = Attr.get_gruop_attr(1).where(is_default: true)
+    #发动机
+    @attrs_three = Attr.get_gruop_attr(2).where(is_default: true)
+    #变速箱
+    @attrs_four = Attr.get_gruop_attr(3).where(is_default: true)
+    #底盘转向
+    @attrs_five = Attr.get_gruop_attr(4).where(is_default: true)
+    #车轮制动
+    @attrs_six = Attr.get_gruop_attr(5).where(is_default: true)
+    #重要配置
+    @attrs_seven = Attr.get_gruop_attr(6).where(is_default: true)
+    #更多配置
+    @attrs_eight = Attr.get_gruop_attr(7).where(is_default: true)
+  end
+
+  def create
+    good = Good.new(good_pramas)
+    if good.save
+    end
+  end
+
+  private
+  def good_pramas
+    params.require(:good).permit(:name, :source_from,:current_price, :market_price,:registered_info,:in_stock,:status)
   end
 end
