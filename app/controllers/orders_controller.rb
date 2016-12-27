7e3bb519803a355ba3bab704aece629b1d85f601
FoxZhao <395381480@qq.com>
2016-12-27 16:07:37 +0800

    初步完成预约下单功能

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,27 @@
+class OrdersController < ApplicationController
+  layout 'frontend'
+
+  def new
+    @good = Good.includes(good_attr_options: { attr_option: :attr }).find(params[:good_id])
+    @configrations = @good.highlight_configrations
+    @image = Good.photos(@good.photo_ids)[0]
+  end
+
+  def create
+    order = Order.new(
+      user_id: 1,
+      good_id: params[:good_id],
+      name: params[:order][:name],
+      phone_num: params[:order][:phone_num],
+      province_id: params[:order][:province_id],
+      city_id: params[:order][:city_id],
+      requirements: params[:order][:requirements],
+      status: 1
+      )
+    if order.save
+      redirect_to personal_path
+    else
+      render 'new'
+    end
+  end
+end
