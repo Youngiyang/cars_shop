ed1696ff47f9f45f6ded31dd7183bb0753689ed5
= <274173368@qq.com>
2016-12-30 15:01:44 +0800

    不知道添加了啥

--- app/controllers/details_controller.rb
+++ app/controllers/details_controller.rb
@@ -2,6 +2,38 @@ class DetailsController < ApplicationController
   layout "frontend"
   
   def index
-    @good = Good.find(params[:id]).specs
+    # @id = params[:id]
+    # # product
+    # @product = Good.find(params[:id]).product
+    # # goods
+    # @goods = product.goods
+
+
+    good = Good.includes(product: { goods: [{ good_spec_options: { spec_option: :spec } }, { good_attr_options: { attr_option: :attr } }] }).find(params[:id])
+        product = good.product
+        goods = product.goods
+        return_hash = {}
+        return_hash['product'] = product.entity
+        return_hash['goods'] = []
+        goods.each do |good|
+          return_hash['goods'].append(good.entity)
+        end
+        @return_hash =return_hash
+
+
+    # @good = Good.find(params[:id]).specs
+
+    # good = Good.includes(product: { goods: [{ good_spec_options: { spec_option: :spec } }, { good_attr_options: { attr_option: :attr } }] }).find(params[:id])
+    #     pp product = good.product
+    #     goods = product.goods
+    #     @count = goods.count
+    #     return_hash = {}
+    #     @product = product.entity
+    #     return_hash['goods'] = []
+    #     goods.each do |good|
+    #       return_hash['goods'].append(good.entity)
+    #     end
+    #     @return_hash = return_hash
+    #     binding.pry
   end
 end
